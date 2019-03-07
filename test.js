7818328ab2fcb6205826d91320e85d0cb1ac1bab
gauthierbal <gauthierbal@gmail.com>
2019-03-07 14:41:25 +0100

    map geolocalisation

---
+++ test.js
@@ -0,0 +1,21 @@
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 6
+  });
+  infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.open(map);
+      map.setCenter(pos);
+    });
+  }
+}
